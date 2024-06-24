e29dcbaf213e9954c6add0c097da714bdbfb2bf1
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-06-25 01:57:14 +0530

    Success to dermatologist page create
    User show each treatment details all screens correctly.

--- frontend/src/screens/DermatologistScreen.jsx
+++ frontend/src/screens/DermatologistScreen.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../css/Dermatologist.css';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const DermatologistScreen = () => {
-  return <div>DermatologistScreen</div>;
+  const [isClicked, setIsClicked] = useState(false);
+  // handle information function
+  const handleInformation = () => {
+    setIsClicked(true);
+  };
+  return (
+    <div className="dermatologistContainer">
+      <div className="informationToggle">
+        <button
+          className={`${isClicked && 'hiddenBtn'}`}
+          type="button"
+          onClick={handleInformation}
+        >
+          Read Details
+        </button>
+
+        <AiFillCloseCircle
+          className={`${isClicked && 'visibleClose'} closeBtn`}
+          onClick={() => setIsClicked(false)}
+        />
+      </div>
+
+      <div className={`${isClicked && 'hiddenInfo'} infoTreatment`}>
+        <h1>Treatment Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          corrupti qui ea optio velit, iste fugiat ex a minima, dolores,
+          necessitatibus sint harum. Distinctio, omnis! Ratione, excepturi eos.
+          Debitis, soluta.
+        </p>
+        <button type="button">Find your Dermatologist</button>
+      </div>
+    </div>
+  );
 };
 
 export default DermatologistScreen;
