3f003585cf07f8a64aecdf9e09714e12b9cfe32e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-07-03 00:30:07 +0530

    Add table structure to show all the doctors

--- frontend/src/screens/AllDermatologistsScreen.jsx
+++ frontend/src/screens/AllDermatologistsScreen.jsx
@@ -7,14 +7,28 @@ const AllDermatologistsScreen = () => {
     e.preventDefault();
   };
   return (
-    <div className="container-fluid">
+    <div>
       <div className="doctorsMainSection">
         <form onSubmit={searchDermatologists} className="searchForm">
           <button type="submit">search</button>
           <input placeholder="Search Dermatologist" type="text" />
         </form>
       </div>
-      <div className="allDoctors"></div>
+      <div className="allDoctors">
+        <div className="tableSection">
+          <table className="docTable">
+            <thead>
+              <td>Name</td>
+              <td>Time</td>
+            </thead>
+
+            <tbody>
+              <td>Dr. Michel Jonathan</td>
+              <td>3.00pm - 6.00pm</td>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
