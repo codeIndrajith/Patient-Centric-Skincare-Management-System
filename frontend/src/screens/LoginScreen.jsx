1ec062ac09b44eaebba3445940e2a156aa5f8649
Indrajith Bodhinayaka <indrajith.bodhinayaka@arimaclanka.com>
2024-11-24 10:40:20 +0530

    Change style in login page

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import FormContainer from "../components/FormContainer";
+import Authentication from '../images/Authentication.gif'
 import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/usersApiSlice";
 import { setCredentials } from "../slices/authSlice";
-import { toast } from "react-toastify";
+import { toast } from "react-hot-toast";
 import Loader from "../components/Loader";
+import '../css/Login.css'
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -32,53 +32,66 @@ const LoginScreen = () => {
       dispatch(setCredentials({ ...res }));
       navigate("/questionnaire");
     } catch (err) {
-      toast.error(err?.data?.message || err.error);
+      toast.error(`${err?.data?.message || err.error}`);
     }
   };
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
+    <div className="signin-container">
+      <div className="illustration-container">
+        <img
+          src={Authentication}
+          alt="Illustration"
+          className="illustration-img"
+        />
+      </div>
 
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="my-2" controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+      <div className="form-container">
+        <h1 className="form-header">Wellcome</h1>
 
-        <Form.Group className="my-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+        <form onSubmit={submitHandler} className="form">
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+            />
+          </div>
 
-        <Button
-          disabled={isLoading}
-          type="submit"
-          variant="primary"
-          className="mt-3"
-        >
-          Sign In
-        </Button>
-      </Form>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+            />
+          </div>
 
-      {isLoading && <Loader />}
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="btn-submit"
+          >
+            Sign In
+          </button>
+        </form>
 
-      <Row className="py-3">
-        <Col>
-          New Customer? <Link to="/register">Register</Link>
-        </Col>
-      </Row>
-    </FormContainer>
+        {isLoading && <div className="loader"><Loader /></div>}
+
+        <div className="register-link">
+          <p>
+            New Customer? <Link to="/register">Register</Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
