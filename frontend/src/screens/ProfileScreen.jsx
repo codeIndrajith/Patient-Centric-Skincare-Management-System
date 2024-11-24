92322ac28524bd7d6008a6406cef977399c25014
Indrajith Bodhinayaka <indrajith.bodhinayaka@arimaclanka.com>
2024-11-24 13:07:39 +0530

    Change style in profile update screen

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -3,11 +3,11 @@ import { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
-import { toast } from 'react-toastify';
+import toast from 'react-hot-toast';
 import Loader from '../components/Loader';
 import { useUpdateUserMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
-import profileImage from '../images/Update.svg'
+import update from '../images/update.gif'
 import '../css/Profile.css'
 
 const ProfileScreen = () => {
@@ -43,75 +43,76 @@ const ProfileScreen = () => {
         dispatch(setCredentials(res));
         toast.success('Profile updated successfully');
       } catch (err) {
-        toast.error(err?.data?.message || err.error);
+        toast.error(`${err?.data?.message || err.error}`);
       }
     }
   };
   return (
     <div className='profileContainer'>
-      <div className='profileImageSection'>
-      <img src={profileImage} alt="profileImage" />
-    </div>
-      <div className="formSec">
-        <h1>Update Profile</h1>
+  <div className='profileImageSection'>
+    <img src={update} alt="update" />
+  </div>
+  <div className="formSec">
+    <h1>Update Profile</h1>
 
-        <form onSubmit={submitHandler}>
-          <div className="form-group my-2">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Enter name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="form-control"
-            />
-          </div>
-
-          <div className="form-group my-2">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-            />
-          </div>
-
-          <div className="form-group my-2">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-            />
-          </div>
-
-          <div className="form-group my-2">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              placeholder="Confirm password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="form-control"
-            />
-          </div>
-
-          <button type="submit" className="update-btn btn-primary mt-3">
-            Update
-          </button>
-
-          {isLoading && <Loader />}
-        </form>
+    <form onSubmit={submitHandler}>
+      <div className="form-group my-2">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="form-control"
+        />
       </div>
-    </div>
+
+      <div className="form-group my-2">
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group my-2">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group my-2">
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          placeholder="Confirm password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <button type="submit" className="update-btn mt-3">
+        Update
+      </button>
+
+      {isLoading && <Loader />}
+    </form>
+  </div>
+</div>
+
   );
 };
 
